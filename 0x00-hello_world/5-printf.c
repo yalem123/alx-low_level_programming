a33e995fe504014a2ffd0be05d5a40b1cf11c48c
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-07 16:34:50 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,9 @@
  * return: Always 0 (Success)
  */
 int main(void)
+	/**
+         * Description - This is a description of printif
+         */
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
