eaf0b13fbc140c96b502258e741520ce9c9838f6
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-07 16:54:36 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-	/**
-         * Description - This is a description of printif
-         */
-
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,/n");
 return (0);
 }
