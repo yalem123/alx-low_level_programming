464128071cb9e852d30ac70ea54e7d2f8b4c35c2
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-08 18:54:46 -0700

    print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - prints all possible different combinations of two digits
+ * Return: 0 Always
+ */
+ int main(void)
+{
+int dgt1, dgt2;
+for (dgt1 = 0; dgt1 <= 9; dgt1++)
+{
+for (dgt2 = dgt1 + 1; dgt2 <= 10; dgt2++)
+{
+putchar((dgt1 % 10) + '0');
+putchar((dgt2 % 10) + '0');
+if (dgt1 == 8 && dgt2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
+ 
