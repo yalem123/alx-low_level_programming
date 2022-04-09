7e5b4272b3279edb72fac129eab334aa5f7cb918
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-08 20:00:34 -0700

    print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int dgt1, dgt2;
-for (dgt1 = 0; dgt1 <= 9; dgt1++)
+for (dgt1 = 0; dgt1 < 9; dgt1++)
 {
-for (dgt2 = dgt1 + 1; dgt2 <= 10; dgt2++)
+for (dgt2 = dgt1 + 1; dgt2 < 10; dgt2++)
 {
 putchar((dgt1 % 10) + '0');
 putchar((dgt2 % 10) + '0');
-if (dgt1 == '8' && dgt2 == '9')
+if (dgt1 == 8 && dgt2 == 9)
 continue;
 putchar(',');
 putchar(' ');
