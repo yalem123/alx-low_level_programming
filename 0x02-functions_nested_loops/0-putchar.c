3d125e3d2b24784c3e8a7b6736dcda3fca190e9c
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-12 12:09:52 -0700

    print -putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description - print putchar in a new line
+ * Return: 0 Always
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
