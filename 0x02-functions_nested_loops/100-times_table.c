211348c83d585089a8563a3f5e6dd0efbe4f4c97
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-12 17:14:52 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ if (prod >= 100)
 _putchar((prod / 100) + '0');
 _putchar((prod / 100) % 10 + '0');
 }
-else if (prod < = 99 && prod >= 10)
+else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 _putchar((prod / 10) % 10 + '0');
