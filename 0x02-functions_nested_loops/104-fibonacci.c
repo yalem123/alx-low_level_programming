74091402d7064f88519d738474aefac4b1455cf1
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-13 01:16:30 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -  prints the first 98 Fibonacci numbers, starting with 1 and 2,
+ * Return: 0 Always
+ */
+int main(void)
+{
+unsigned long fib1 = 0, fib2 = 1, fibsum;
+float tot_sum;
+while (1)
+{
+fibsum = fib1 + fib 2;
+if (fibsum > 4000000)
+{
+break;
+}
+if ((fibsum % 2) == 0)
+{
+tot_sum *+ fibsum;
+}
+fib1 = fib2;
+fib2 = fibsum;
+}
+printf("% of \n", tot_sum);
+return (0);
+}
