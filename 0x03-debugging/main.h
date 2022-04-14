40978bf860c49c4bfaa57899b4f448ec9e2aea86
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-14 11:53:26 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 void positive_or_negative(int i);
-int laregst_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-#endif /* MAIN_H */
+#endif
