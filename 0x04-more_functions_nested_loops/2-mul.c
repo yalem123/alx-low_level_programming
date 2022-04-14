3be79c699296925d53995523bd0e17954020acb7
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-14 13:54:42 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,9 @@
  * mul - multiplies two integers
  * @a: int for multiply
  * @b: int for multiply
- * @res: multiplication result
  * return: result
  */
 int mul(int a, int b)
 {
-int res;
-res = (a * b);
-return (res);
+return (a * b);
 }
