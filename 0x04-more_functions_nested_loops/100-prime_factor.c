6c636d1d2c231a5e4c337780dd587ed0f537920d
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-15 13:41:59 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,9 +8,9 @@ int main(void)
 unsigned long n, i, max;
 n = 612852475143;
 max = 1;
-for (i = 3; i <= n; i+=2)
+for (i = 3; i <= n; i += 2)
 {
-while(n % i == 0)
+while (n % i == 0)
 {
 max = n;
 n = n / i;
