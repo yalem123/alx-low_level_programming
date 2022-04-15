7ff320e2ed923b08b4175a95e9364dd621d56ecf
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-15 14:42:40 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -14,6 +14,6 @@ day = 29;
 year = 2000;
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 day = convert_day(month, day);
-printing_remaining_days(month, day, year);
+print_remaining_days(month, day, year);
 return (0);
 }
