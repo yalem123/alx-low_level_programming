a5adb6b0be2c217124dfa0d8b979100696f3a434
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-14 17:03:03 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print fizz buzz
+ * Return: 0 
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("FIZZ");
+}
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf("BUZZ");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FIZZBUZZ");
+{
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+_putchar(' ');
+else
+_putchar('\n');
+}
+return (0);
+}
