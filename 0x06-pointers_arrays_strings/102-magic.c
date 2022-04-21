7e54a9cf9ea958099eeff9fac392173006e6274e
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-21 12:29:25 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a[2] = 98
+ * Return: 0 Always
+ */
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+p[5] = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
