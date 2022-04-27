188933c9908f288da1f761b3bb28ba3e0f421361
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-27 15:29:06 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar('*s');
+_puts_print_rev_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
