2fd6765e706de7b0bff49218fc68914cd71f275e
yalem123 <yalembrhankelayneh@gmail.com>
2022-04-27 16:05:27 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x 
+ * @x: integer
+ * @y: integer
+ * Return: the value of exponentation
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
