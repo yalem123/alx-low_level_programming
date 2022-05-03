4e5b646a0b141fd6c2416a2c8f34482cd34a404b
yalem123 <yalembrhankelayneh@gmail.com>
2022-05-02 17:36:48 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - search the number of bytes in the initial
+ * @s:segment targeted
+ * @accept:reference bytes container
+ * Return:returns the number of bytes in the initial
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int bytes = 0;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+bytes++;
+break;
+}
+else if ((accept[i + 1]) == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+Static Library
+static library is a compiled object file containing all symbols required by the main program to operate (functions, variables etc.) 
