a65ed9bf022318de8d4be944ed87482a01bc5147
yalem123 <yalembrhankelayneh@gmail.com>
2022-05-06 09:26:25 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
  * malloc_checked - function that allocates memory using malloc
  * @b: amount of bytes
  * Return: pointer to the allocated memory
+ * if malloc fails, status value is equal to 98.
  */
 void *malloc_checked(unsigned int b)
 {
