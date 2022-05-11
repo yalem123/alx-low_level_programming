ee805fb341f381ec72f49161f142c02916a77df4
yalem123 <yalembrhankelayneh@gmail.com>
2022-05-11 14:57:53 -0700

    function pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: input integer array
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int i;
+if (array && action)
+for (i = 0; i < size; i++)
+action(array[i]);
+}
