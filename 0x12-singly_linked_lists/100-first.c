dc8892608d78101609c2a84a814b5097a40643ba
yalem123 <yalembrhankelayneh@gmail.com>
2022-05-19 12:50:12 -0700

    100 first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - Prints a string 
+ */
+void hare(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
