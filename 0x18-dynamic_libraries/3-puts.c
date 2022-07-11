e8e7584470f605c3bf9c1a035e0b70f1a452c301
yalem123 <yalembrhankelayneh@gmail.com>
2022-07-11 10:23:45 -0700

    advanced

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,7 @@
+#include "main.h"
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+int _abs(int i)
+{
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
+}
